fc8bbd8e3dc1bfed61a3bdf3fa5b27df83d2498c
Christian Greissl <greissl@gdi.de>
2022-12-04 23:17:33 +0100

    Finally Tag 3

--- 2022/Day3_solved.py
+++ 2022/Day3_solved.py
@@ -4,49 +4,54 @@ with open("2022/Day3_input.txt") as f:
     lines =  f.readlines()
 
 
-left = []
-right = []
-for ele in lines:
-    lenght = 0
-    ele = ele.replace("\n", "")
-    length = len(ele)
-    splitter = int(length   /2 )
-    left.append(ele[0:splitter])
-    right.append(ele[splitter:length])
+# left = []
+# right = []
+# for ele in lines:
+#     lenght = 0
+#     ele = ele.replace("\n", "")
+#     length = len(ele)
+#     splitter = int(length   /2 )
+#     left.append(ele[0:splitter])
+#     right.append(ele[splitter:length])
 
-count = -1 
-solution = []
-for ele in left:
-    count += 1
-    max = len(ele)
-    for i in range(max):
-        char = ele[i]
-        if (right[count].find(char) != -1):
-            solution.append(char)
-            break
-print(solution)
-sum = 0
+# count = -1 
+# solution = []
+# for ele in left:
+#     count += 1
+#     max = len(ele)
+#     for i in range(max):
+#         char = ele[i]
+#         if (right[count].find(char) != -1):
+#             solution.append(char)
+#             break
+# print(solution)
+# sum = 0
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' , 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-for ele in solution:
-    for i in range (52):
-        if(letters[i].find(ele) != -1) :
-            sum += i +1 
+# for ele in solution:
+#     for i in range (52):
+#         if(letters[i].find(ele) != -1) :
+#             sum += i +1 
 
-print(sum)
+# print(sum)
 ## part two 
 part2 = []
 for i in range(0,len(lines),3):
-    
     for char in lines[i]:
-        for i in range(len(char)):
-            letter = char[i]
-            if(lines[i+1].find(letter)!= -1):
-                if(lines[i+2].find(letter)!= -1):
-                  part2.append(letter)
+        for j in range(len(lines[i])):
+          
+            if(lines[i+1].find(char)!= -1):
+                if(lines[i+2].find(char)!= -1):
+                    part2.append(char)
+                    break
+parts2 = []
+length = len(part2) -1
+for i in range(length):
+    if (part2[i+1] == "\n"):
+        parts2.append(part2[i])
 
-
+parts2.append(part2[length])
 part2_solution = 0    
-for ele in part2:
+for ele in parts2:
     for i in range (52):
         if(letters[i].find(ele) != -1) :
             part2_solution += i +1
