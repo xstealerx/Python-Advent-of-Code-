34d40ec568fb7bf3da7466c094a9dfc8e1ba525f
Christian Greissl <greissl@gdi.de>
2022-12-08 11:18:06 +0100

    a

--- 2020/01.py
+++ 2020/01.py
@@ -1,12 +1,3 @@
-
-# import os
-
-# cwd = os.getcwd()  # Get the current working directory (cwd)
-# files = os.listdir(cwd)  # Get all the files in that directory
-# print("Files in %r: %s" % (cwd, files))
-
-
-
 numbers = []
 with open("01.txt") as f:
     numbers = list(map(int, f.readlines()))
